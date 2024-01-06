0473a15b3f0f9faa62bd1756f007610c5aa21a00
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2024-01-06 10:25:03 -0300

    feat: create user controller

---
+++ src/presentation/controllers/users/create-user.ts
@@ -0,0 +1,37 @@
+import { CreateUser } from '../../../domain/useCases/users';
+import { EmailValidate, badRequest, ok, serverError } from '../../helpers';
+import { Controller, HttpRequest, HttpResponse } from '../../protocols';
+
+export class CreateUserController implements Controller {
+  constructor(
+    private readonly createUser: CreateUser,
+    private readonly emailValidator: EmailValidate,
+  ) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const requiredFields = ['name', 'email', 'password'];
+
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field])
+          return badRequest(new Error(`O campo ${field} é obrigatório.`));
+      }
+
+      const emailIsValid = this.emailValidator.execute(httpRequest.body.email);
+
+      if (!emailIsValid) {
+        return badRequest(new Error('Email invalido.'));
+      }
+
+      await this.createUser.execute(httpRequest.body);
+
+      return ok({
+        data: {
+          message: 'Usuário criado com sucesso.',
+        },
+      });
+    } catch (error) {
+      return serverError(error);
+    }
+  }
+}
