6a819005667fd2bc9df5caaadb08fe05d0467dce
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2024-01-06 13:11:37 -0300

    feat: create increment access number controller

---
+++ src/presentation/controllers/access/increment.ts
@@ -0,0 +1,21 @@
+import { IncrementAccessNumber } from '../../../domain/useCases/access';
+import { ok, serverError } from '../../helpers';
+import { Controller, HttpResponse } from '../../protocols';
+
+export class IncrementAccessNumberController implements Controller {
+  constructor(private readonly incrementAccessNumber: IncrementAccessNumber) {}
+
+  async handle(): Promise<HttpResponse> {
+    try {
+      await this.incrementAccessNumber.execute();
+
+      return ok({
+        data: {
+          message: 'Número de acessos incrementado.',
+        },
+      });
+    } catch (error) {
+      return serverError(error);
+    }
+  }
+}
