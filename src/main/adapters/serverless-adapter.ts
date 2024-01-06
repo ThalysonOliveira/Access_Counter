1bcdf7b1a3664c3eb98f903e7d5a86c2b35fd875
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2024-01-06 11:08:57 -0300

    feat: create serverless adapter

---
+++ src/main/adapters/serverless-adapter.ts
@@ -0,0 +1,19 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { Controller } from '../../presentation/protocols';
+
+export const serverlessAdapter = (controller: Controller) => {
+  return async (
+    event: APIGatewayProxyEvent,
+  ): Promise<APIGatewayProxyResult> => {
+    const httpRequest = {
+      body: JSON.parse(event.body!),
+    };
+
+    const response = await controller.handle(httpRequest);
+
+    return {
+      statusCode: response.statusCode,
+      body: JSON.stringify(response.body),
+    };
+  };
+};
