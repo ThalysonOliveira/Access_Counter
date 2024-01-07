dead39d8a1e9d4e9d6a8ebf29b18050a1ae1b3be
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2024-01-07 13:23:01 -0300

    feat!: add new return for detail user

--- src/domain/useCases/users/detail-user.ts
+++ src/domain/useCases/users/detail-user.ts
@@ -1,5 +1,5 @@
 import { User } from '../../models';
 
 export interface DetailUser {
-  execute: (userId: string) => Promise<Partial<User>>;
+  execute: (userId: string) => Promise<Partial<User> | null>;
 }
