56eb9403fb012987cc8f9762dc577bc42abc8200
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2024-01-07 14:22:04 -0300

    test: should ensure return bad request if name is not provided

---
+++ src/tests/unit/users/create-user.spec.ts
@@ -0,0 +1,59 @@
+import assert from 'node:assert';
+import { CreateUser } from '../../../domain/useCases/users';
+import { EmailValidate } from '../../../presentation/protocols';
+import { CreateUserController } from '../../../presentation/controllers/users';
+import { badRequest } from '../../../presentation/helpers';
+
+interface SutTypes {
+  sut: CreateUserController;
+  createUser: CreateUser;
+  emailValidate: EmailValidate;
+}
+
+const makeCreateUser = () => {
+  class CreateUserStub implements CreateUser {
+    async execute(): Promise<void> {
+      await new Promise((resolve) => resolve);
+    }
+  }
+
+  return new CreateUserStub();
+};
+
+const makeEmailValidator = () => {
+  class EmailValidatorStub implements EmailValidate {
+    execute(): boolean {
+      return true;
+    }
+  }
+
+  return new EmailValidatorStub();
+};
+
+const makeSut = (): SutTypes => {
+  const createUser = makeCreateUser();
+  const emailValidate = makeEmailValidator();
+  const sut = new CreateUserController(createUser, emailValidate);
+
+  return { createUser, emailValidate, sut };
+};
+
+describe('Create User Controller', () => {
+  it('Should return bad request if name is not provided', async () => {
+    const { sut } = makeSut();
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password',
+      },
+    };
+
+    const response = await sut.handle(httpRequest);
+
+    assert.deepEqual(
+      response,
+      badRequest(new Error('O campo name é obrigatório.')),
+    );
+  });
+});
